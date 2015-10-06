cfe7cc5b7a49d2201e2f9887d3b6b24de0ab5f65
bastien girschig <bastien.girschig@gmail.com>
2015-10-06 11:19:02 +0200

    added alignMode

--- src/Cropper.cpp
+++ src/Cropper.cpp
@@ -8,135 +8,166 @@
 
 #include "Cropper.h"
 
-Cropper::Cropper(){
-    // ??
-    // canvasSettings.internalformat = GL_RGBA16F;
-    
+Cropper::Cropper() {
     // default settings
     canvasSettings.width = 100;
     canvasSettings.height = 100;
     setSource(0, 0, 100, 100);
     setTarget(0, 0, 100, 100);
+    
     debug = false;
+    autoClear = true;
+    crop = true;
+    
     fitMode = FILL;
+    alignMode = CENTER;
 }
 
-void Cropper::set(float sx, float sy, float sw, float sh, float tx, float ty, float tw, float th){
+void Cropper::set(float sx, float sy, float sw, float sh, float tx, float ty, float tw, float th) {
     source.set(sx, sy, sw, sh);
     target.set(tx, ty, tw, th);
 }
 
-void Cropper::begin(){
-    // setting up the canvas size to target width end height
-    if(canvasSettings.width != target.width || canvasSettings.height != target.height){
+void Cropper::begin() {
+    // setting up the canvas size to target width and height
+    if (crop && (canvasSettings.width != target.width || canvasSettings.height != target.height)) {
         canvasSettings.width = target.width; canvasSettings.height = target.height;
         canvas.allocate(canvasSettings);
     }
     
     // begin draw on canvas and clear its previous contents
-    canvas.begin();
+    if (crop) canvas.begin();
     ofPushMatrix();
-    ofClear(255, 255, 255);
+    if (!crop) ofTranslate(target.x, target.y);
+    if(autoClear) ofClear(255, 255, 255);
     
-    // get to the center of the cropper area
-    ofTranslate(target.width/2, target.height/2);
+    // go to the origin (depending on alignMode)
+    if(     alignMode == TOP_LEFT)      ofTranslate(0,                  0);
+    else if(alignMode == TOP_CENTER)    ofTranslate(target.width / 2,   0);
+    else if(alignMode == TOP_RIGHT)     ofTranslate(target.width,       0);
+    else if(alignMode == CENTER_LEFT)   ofTranslate(0,                  target.height / 2);
+    else if(alignMode == CENTER)        ofTranslate(target.width / 2,   target.height / 2);
+    else if(alignMode == CENTER_RIGHT)  ofTranslate(target.width,       target.height / 2);
+    else if(alignMode == BOTTOM_LEFT)   ofTranslate(0,                  target.height);
+    else if(alignMode == BOTTOM_CENTER) ofTranslate(target.width / 2,   target.height);
+    else if(alignMode == BOTTOM_RIGHT)  ofTranslate(target.width,       target.height);
     
     // scale to fit/fill or stretch (depending on the fit mode)
-    if(fitMode == FILL){
+    if (fitMode == FILL) {
         float scale;
-        if(source.getAspectRatio() > target.getAspectRatio()) scale = (float)target.height / source.height;
+        if (source.getAspectRatio() > target.getAspectRatio()) scale = (float)target.height / source.height;
         else scale = (float)target.width / source.width;
         ofScale(scale, scale);
     }
-    else if(fitMode == FIT){
+    else if (fitMode == FIT) {
         float scale;
-        if(source.getAspectRatio() > target.getAspectRatio()) scale = target.width / source.width;
+        if (source.getAspectRatio() > target.getAspectRatio()) scale = target.width / source.width;
         else scale = target.height / source.height;
         ofScale(scale, scale);
     }
-    else if (fitMode == STRETCH){
-        ofScale(target.width/source.width, target.height/source.height);
+    else if (fitMode == STRETCH) {
+        ofScale(target.width / source.width, target.height / source.height);
     }
     
     // center the source rectangle inside the cropper
-    ofTranslate(-source.width/2-source.x, -source.height/2-source.y);
+    if(     alignMode == TOP_LEFT)      ofTranslate(-source.x,                      -source.y);
+    else if(alignMode == TOP_CENTER)    ofTranslate(-source.width / 2 - source.x,   -source.y);
+    else if(alignMode == TOP_RIGHT)     ofTranslate(-source.width - source.x,       -source.y);
+    else if(alignMode == CENTER_LEFT)   ofTranslate(-source.x,                      -source.height / 2 - source.y);
+    else if(alignMode == CENTER)        ofTranslate(-source.width / 2 - source.x,   -source.height / 2 - source.y);
+    else if(alignMode == CENTER_RIGHT)  ofTranslate(-source.width - source.x,       -source.height / 2 - source.y);
+    else if(alignMode == BOTTOM_LEFT)   ofTranslate(-source.x,                      -source.height - source.y);
+    else if(alignMode == BOTTOM_CENTER) ofTranslate(-source.width / 2 - source.x,   -source.height - source.y);
+    else if(alignMode == BOTTOM_RIGHT)  ofTranslate(-source.width - source.x,       -source.height - source.y);
     
 }
 
-void Cropper::end(){
+void Cropper::end() {
+    // draw debug part one (using cropper's transformations)
+    if (debug) drawDebug(0);
+    
     // reset transformations
     ofPopMatrix();
-
-    // draw debug on top of eveything, outside of any transformation
-    if(debug) drawDebug();
     
-    // end canvas and draw it
-    canvas.end();
-}
-void Cropper::draw(){
-    canvas.draw(target.x, target.y);
-}
-
-void Cropper::drawDebug(){
-    string debugInfo;
+    // draw debug part 2 (not using transformations)
+    if (debug) drawDebug(1);
     
-    debugInfo += "fitmode: ";
-    if(fitMode==FIT) debugInfo += "Fit";
-    else if(fitMode==STRETCH) debugInfo += "Stretch";
-    else if(fitMode==FILL) debugInfo += "Fill";
-    debugInfo += "\n\n";
-    debugInfo += "source position: " + ofToString(source.x) + ", " +ofToString(source.y)+"\n";
-    debugInfo += "source shape: " + ofToString(source.width) + ", " +ofToString(source.height);
-    debugInfo += "\n\n";
-    debugInfo += "cropper position: " + ofToString(target.x) + ", " +ofToString(target.y)+"\n";
-    debugInfo += "cropper shape: " + ofToString(target.width) + ", " +ofToString(target.height);
+    // end canvas
+    if (crop) canvas.end();
+}
+void Cropper::draw() {
+    if (crop) canvas.draw(target.x, target.y);
+    else ofLog(OF_LOG_WARNING) << "cropper: this cropper's crop value is set to false. there is no need for the draw() function";
+}
 
+void Cropper::drawDebug(int part) {
     ofSetColor(255);
     ofPushStyle();
     ofEnableAlphaBlending();
     
-    // fill cropper area with translucent color
-    ofSetColor(255, 0, 0, 80); ofFill(); ofRect(0, 0, target.width, target.height);
-    // mark cropper's boundaries
-    ofSetColor(255, 0, 0); ofNoFill(); ofRect(0, 0, target.width, target.height);
-    // diagonals
-    ofLine(0, 0, target.width, target.height);
-    ofLine(0, target.height, target.width, 0);
-    
-    // debug text background
-    ofFill(); ofSetColor(255,255,255,180); ofRect(0,0,250,130);
-    // debug text
-    ofSetColor(0); ofDrawBitmapString(debugInfo, ofPoint(10,18));
-    
+    if (part == 0) { // to be drawn using the cropper's transformation
+        // mark cropper's boundaries and diagonals
+        ofSetColor(0, 0, 255); ofNoFill();
+        ofRect(0, 0, source.width, source.height);
+        ofLine(0, 0, source.width, source.height);
+        ofLine(0, source.height, source.width, 0);
+    }
+    else if (part == 1) { // to be drawn without the cropper's transformations
+        string debugInfo;
+        
+        debugInfo += "fitmode: ";
+        if (fitMode == FIT) debugInfo += "Fit";
+        else if (fitMode == STRETCH) debugInfo += "Stretch";
+        else if (fitMode == FILL) debugInfo += "Fill";
+        debugInfo += "\n\n";
+        debugInfo += "source position: " + ofToString(source.x) + ", " + ofToString(source.y) + "\n";
+        debugInfo += "source shape: " + ofToString(source.width) + ", " + ofToString(source.height);
+        debugInfo += "\n\n";
+        debugInfo += "cropper position: " + ofToString(target.x) + ", " + ofToString(target.y) + "\n";
+        debugInfo += "cropper shape: " + ofToString(target.width) + ", " + ofToString(target.height);
+        
+        // fill cropper area with translucent color
+        ofSetColor(255, 0, 0, 20); ofFill(); ofRect(0, 0, target.width, target.height);
+        // mark cropper's boundaries and diagonals
+        ofSetColor(255, 0, 0); ofNoFill();
+        ofRect(0, 0, target.width, target.height);
+        ofLine(0, 0, target.width, target.height);
+        ofLine(0, target.height, target.width, 0);
+        
+        // debug text background
+        ofFill(); ofSetColor(255, 255, 255, 180); ofRect(0, 0, 250, 130);
+        // debug text
+        ofSetColor(0); ofDrawBitmapString(debugInfo, ofPoint(10, 18));
+    }
     ofPopStyle();
 }
 
-ofRectangle* Cropper::getSource(){ return &source; }
-ofRectangle* Cropper::getTarget(){ return &target; }
+ofRectangle* Cropper::getSource() { return &source; }
+ofRectangle* Cropper::getTarget() { return &target; }
 
-void Cropper::setFitMode(FitMode mode){ fitMode = mode;}
-Cropper::FitMode Cropper::getFitMode(){ return fitMode; }
+void Cropper::setFitMode(FitMode mode) { fitMode = mode; }
+Cropper::FitMode Cropper::getFitMode() { return fitMode; }
 
 // target handlers
-void Cropper::setTarget(float x, float y, float w, float h){
+void Cropper::setTarget(float x, float y, float w, float h) {
     target.set(x, y, w, h);
 }
-void Cropper::setTargetShape(float w, float h){
+void Cropper::setTargetShape(float w, float h) {
     target.width = w;
     target.height = h;
 }
-void Cropper::setTargetPos(float x, float y){
+void Cropper::setTargetPos(float x, float y) {
     target.setPosition(x, y);
 }
 
 // source handlers
-void Cropper::setSource(float x, float y, float w, float h){
+void Cropper::setSource(float x, float y, float w, float h) {
     source.set(x, y, w, h);
 }
-void Cropper::setSourceShape(float w, float h){
+void Cropper::setSourceShape(float w, float h) {
     source.width = w;
     source.height = h;
 }
-void Cropper::setSourcePos(float x, float y){
+void Cropper::setSourcePos(float x, float y) {
     source.setPosition(x, y);
 }
--- src/Cropper.h
+++ src/Cropper.h
@@ -5,58 +5,78 @@
 //
 //
 
+// TODO: use matrices instead of succesive transforms and scales.
+// save the original one and compute the transformed one when a parameter is modified.
+
 #ifndef __cropperApp__Cropper__
 #define __cropperApp__Cropper__
 
 #include "ofMain.h"
 
 
-class Cropper{
-    public:
-        Cropper();
+class Cropper {
+public:
+    Cropper();
     
-        ofFbo canvas;
-        void begin();
-        void end();
-        void draw();
+    ofFbo canvas;
+    void begin();
+    void end();
+    void draw();
     
-        // set all parameters at once
-        void set(float sx, float sy, float sw, float sh, float tx, float ty, float tw, float th);
-        
-        // target handlers
-        void setTarget(float x, float y, float w, float h);
-        void setTargetShape(float w, float h);
-        void setTargetPos(float x, float y);
-        
-        // source handlers
-        void setSource(float x, float y, float w, float h);
-        void setSourceShape(float w, float h);
-        void setSourcePos(float x, float y);
+    // set all parameters at once
+    void set(float sx, float sy, float sw, float sh, float tx, float ty, float tw, float th);
     
-        // draws debug info if true
-        bool debug;
+    // target handlers
+    void setTarget(float x, float y, float w, float h);
+    void setTargetShape(float w, float h);
+    void setTargetPos(float x, float y);
     
-        // fitmodes
-        enum FitMode{
-            FILL,
-            FIT,
-            STRETCH
-        };
+    // source handlers
+    void setSource(float x, float y, float w, float h);
+    void setSourceShape(float w, float h);
+    void setSourcePos(float x, float y);
+    
+    // draws debug info if true
+    bool debug;
+    // clears fbo on begin if true
+    bool autoClear;
+    // if false, no FBO is used. this only resizes the "contents"
+    bool crop;
+    
+    // fitmodes
+    enum FitMode {
+        FILL,
+        FIT,
+        STRETCH
+    };
+    // align modes
+    enum AlignMode{
+        TOP_LEFT,
+        TOP_CENTER,
+        TOP_RIGHT,
+        CENTER_LEFT,
+        CENTER,
+        CENTER_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_CENTER,
+        BOTTOM_RIGHT
+    };
+    AlignMode alignMode;
     
     
-        // getters & setters
-        ofRectangle* getTarget();
-        ofRectangle* getSource();
-        void setFitMode(FitMode mode);
-        FitMode getFitMode();
+    // getters & setters
+    ofRectangle* getTarget();
+    ofRectangle* getSource();
+    void setFitMode(FitMode mode);
+    FitMode getFitMode();
     
-    private:
-        ofFbo::Settings canvasSettings;
-        ofRectangle source;
-        ofRectangle target;
-        FitMode fitMode;
+private:
+    ofFbo::Settings canvasSettings;
+    ofRectangle source;
+    ofRectangle target;
+    FitMode fitMode;
     
-        void drawDebug();
+    void drawDebug(int part);
 };
 
 #endif /* defined(__cropperApp__Cropper__) */
